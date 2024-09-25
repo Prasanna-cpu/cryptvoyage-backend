8884ac456bdb3e95074cbb499d614bfa4a656b7b
Prasanna Kumar <kumar22maran@gmail.com>
2024-09-25 07:33:08 +0530

    Commit

---
+++ src/main/java/com/kumar/backend/Service/Abstraction/WalletService.java
@@ -0,0 +1,21 @@
+package com.kumar.backend.Service.Abstraction;
+
+import com.kumar.backend.Exception.InsufficientBalanceException;
+import com.kumar.backend.Exception.NonExistentWalletException;
+import com.kumar.backend.Model.Order;
+import com.kumar.backend.Model.User;
+import com.kumar.backend.Model.Wallet;
+
+public interface WalletService {
+    Wallet getUserWallet(User user);
+
+    Wallet addBalanceToWallet(Wallet wallet,Long money);
+
+    Wallet findWalletById(Long id) throws NonExistentWalletException;
+
+    Wallet wallletToWalletTransfer(User sender,Wallet recieverWallet,Long amount) throws InsufficientBalanceException;
+
+    Wallet orderPayment(Order order,User user);
+
+
+}
